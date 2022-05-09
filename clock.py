46871af288a17573725b9c598dceac76be096f83
Sam Fortuin <99059050@mydavinci.nl>
2022-05-09 10:52:17 +0200

    finished clock

--- clock.py
+++ clock.py
@@ -3,17 +3,21 @@ import tkinter as tk
 
 def updateClock():
     grabtime = str(datetime.now())
-    return grabtime[11:19]
+    stringVar.set(grabtime[11:19])
+    window.after(afterTime,updateClock)
 
 #init
 window = tk.Tk()
 window.title('Clock')
 
+afterTime=500
+stringVar = tk.StringVar(value='')
+
 label = tk.Label(window)
-label.configure(text=updateClock())
+label.configure(textvariable=stringVar,font='ariel 80',bg='cyan',fg='blue')
 label.pack()
 
-# window.after(1000,label.configure(text=updateClock()))
+window.after(afterTime,updateClock)
 
 window.mainloop()
 
