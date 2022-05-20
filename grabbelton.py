9dfb9dbe22380078c5de6e456ffbf623cf9e466b
Sam Fortuin <99059050@mydavinci.nl>
2022-05-20 21:55:13 +0200

    a

--- grabbelton.py
+++ grabbelton.py
@@ -1,44 +1,41 @@
 import tkinter as tk
-from random import randint
-
+from random import randint,choice
 
 #globals
-toyArray = ['Barbie','Hotwheels','Pokemon kaart']
+toyArray = ['Jojo','Potloden','Stuiterbal','Knikkers','Bellenblaas','Tol','Stickers','Gum','Dinosaurus','Glitterpen','Springtouw','Sleutelhanger']
 grabbedToysArray =[]
 bgColor = '#90EE90'
 
-def grabbelCount():
-    a = randint(0,len(toyArray)-1)
-    grabbedToysArray.append(toyArray[a])
-    items.insert(tk.END,grabbedToysArray[len(grabbedToysArray)-1])
-    del toyArray[a]
-    toyAmount.set(len(toyArray))
-    grabbedToys.set(grabbedToysArray)
-    None if len(toyArray) > 0 else btn.configure(state='disabled',textvariable=emptyBoy)
+def grabbelJongen(disable=False):
+    if not disable:
+        a = choice(toyArray)
+        removeToy = toyArray.index(a)
+        grabbedToysArray.append(toyArray[removeToy])
+        print(grabbedToysArray)
+        del toyArray[removeToy]
+        yots.set(value=toyArray)
 
+    #switch instead of if cause learning is good
+    match len(toyArray):
+        case 0:
+            lbl.destroy()
+            btn.configure(state="disabled")
 
-root = tk.Tk()
-root.geometry("1280x720")
-root.title('Grabbelton')
-root.configure(bg=bgColor)
+if __name__ == '__main__':
+    root = tk.Tk()
+    root.geometry("1280x720")
+    root.title('Grabbelton')
+    root.configure(bg=bgColor)
 
-grabbedToys = tk.StringVar(value=grabbedToysArray)
-toyAmount = tk.StringVar(value=len(toyArray))
-emptyBoy = tk.StringVar(value='De ton is leeg')
+    yots = tk.StringVar(value=toyArray)
 
-btn = tk.Button(root)
-btn.configure(textvariable=toyAmount,font='ariel 50',command=grabbelCount)
-btn.place(relx=0.5,rely=0.3,anchor='center')
+    btn = tk.Button(root)
+    btn.configure(text='Grabbel',font='ariel 50',command=grabbelJongen)
+    btn.place(relx=0.5,rely=0.5,anchor='center')
+    grabbelJongen(True)
 
-label = tk.Label(root)
-label.configure(text='gegrabbelde items',font='ariel 20',bg=bgColor)
-label.place(relx=0.5,rely=0.5,anchor='center')
+    lbl = tk.Label(root)
+    lbl.configure(textvariable=yots)
+    lbl.place(relx=0.2,rely=0.2,anchor='center')
 
-items = tk.Text(root)
-items.tag_configure('centerB',justify='center')
-items.insert(tk.END,'bebis')
-items.configure(state='disabled',width=8,height=2,font='ariel 50')
-items.tag_add('centerB','1.0','end')
-items.place(relx=0.5,rely=0.7,anchor='center')
-
-root.mainloop()
+    root.mainloop()
--- test.py
+++ test.py
@@ -1,6 +1,7 @@
 import string,os,sys,random
 from time import sleep
 from shortcuts import *
+from grabbelton import toyArray
 
 if __name__ == '__main__':
-    
+    print(len(toyArray))
