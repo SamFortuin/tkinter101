d4c267189cbf69fe22aaee136ea43f6fa6a57936
Sam Fortuin <99059050@mydavinci.nl>
2022-06-09 10:16:40 +0200

    finished barrel

--- grabbelton.py
+++ grabbelton.py
@@ -2,24 +2,29 @@ import tkinter as tk
 from random import randint,choice
 
 #globals
-toyArray = ['Jojo','Potloden','Stuiterbal','Knikkers','Bellenblaas','Tol','Stickers','Gum','Dinosaurus','Glitterpen','Springtouw','Sleutelhanger']
+toyArray = ['Jojo','Potlood','Stuiterbal','Knikker','Bellenblaas','Tol','Sticker','Gum','Dinosaurus','Glitterpen','Springtouw','Sleutelhanger']
 grabbedToysArray =[]
 bgColor = '#90EE90'
 
 def grabbelJongen(disable=False):
+    if len(grabbedToysArray) < 1 and not disable:
+        lbl = tk.Label(root)
+        lbl.configure(textvariable=yots,font='ariel 30',bg=bgColor)#,fg='white')
+        lbl.place(relx=0.5,rely=0.6,anchor='center')
+
     if not disable:
         a = choice(toyArray)
         removeToy = toyArray.index(a)
-        grabbedToysArray.append(toyArray[removeToy])
+        grabbedToysArray.append(toyArray[removeToy])#+'\n')
         print(grabbedToysArray)
         del toyArray[removeToy]
-        yots.set(value=toyArray)
+        yots.set(value=f'U heeft een {grabbedToysArray[len(grabbedToysArray)-1]} gegrabbeld')
 
     #switch instead of if cause learning is good
     match len(toyArray):
         case 0:
-            lbl.destroy()
-            btn.configure(state="disabled")
+            # lbl.destroy()
+            btn.configure(state="disabled",text='Ton is leeg')
 
 if __name__ == '__main__':
     root = tk.Tk()
@@ -27,15 +32,11 @@ if __name__ == '__main__':
     root.title('Grabbelton')
     root.configure(bg=bgColor)
 
-    yots = tk.StringVar(value=toyArray)
+    yots = tk.StringVar(value=grabbedToysArray)
 
     btn = tk.Button(root)
     btn.configure(text='Grabbel',font='ariel 50',command=grabbelJongen)
-    btn.place(relx=0.5,rely=0.5,anchor='center')
+    btn.place(relx=0.5,rely=0.4,anchor='center')
     grabbelJongen(True)
-
-    lbl = tk.Label(root)
-    lbl.configure(textvariable=yots)
-    lbl.place(relx=0.2,rely=0.2,anchor='center')
 
     root.mainloop()
